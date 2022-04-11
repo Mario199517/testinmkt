0348cd5775b6cb5878b62d8186985395a1cc3ae6
Agustin <agusos9728@gmail.com>
2022-04-11 17:44:45 -0500

    Aguz: agregue un script de sumatoria

--- index.php
+++ index.php
@@ -13,8 +13,18 @@
             <input id="uno" type="text">
             <input id="dos" type="text">
             <input id="result" type="text">
+            <button id="btn">Ver resultado</button>
         </div> 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script src="" async defer></script>
+        <script>
+            $(document).ready(function() {
+                $("#btn").on('click', function() {
+                    var uno = $("#uno").val();
+                    var dos = $("#dos").val();
+                    var suma = parseInt(uno) + parseInt(dos);
+                    $("#result").val(suma);
+                });
+            });
+        </script>
     </body>
 </html>
