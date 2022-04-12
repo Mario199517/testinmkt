407d712f7e64659f408da411e2bebf6126019103
RichardGcia <richardgcia04@gmail.com>
2022-04-11 21:34:20 -0500

    Agregue carpetas y descargue Jquery para tenerlo en el proyecto

--- index.php
+++ index.php
@@ -17,7 +17,8 @@
             <input id="result" type="text">
             <button id="btn">Ver resultado</button>
         </div> 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="assets/js/jquery.js"></script> 
+
         <script>
             $(document).ready(function() {
                 $("#btn").on('click', function() {
