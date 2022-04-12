48dba83c768d8d129912aa9bd719ef8c485ffb8c
RichardGcia <richardgcia04@gmail.com>
2022-04-11 22:00:48 -0500

    Agregue Bootstrap

--- index.php
+++ index.php
@@ -6,18 +6,20 @@
         <title>_TEST con GIT</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="vendors/bootstrap/css/bootstrap.css">
     </head>
     <body>
         <h1>No agarra el fetch de agus :v</h1>
         <h2>HOLA MUNDO EN JAVA</h2>
         <div>
-            <input id="uno" type="text">
-            <input id="dos" type="text">
-            <input id="result" type="text">
-            <button id="btn">Ver resultado</button>
+            <input id="uno" type="text" class="form-control">
+            <input id="dos" type="text" class="form-control">
+            <input id="result" type="text" class="form-control">
+            <button id="btn" class="btn btn-primary">Ver resultado</button>
         </div> 
-        <script src="assets/js/jquery.js"></script> 
+
+        <script src="vendors/bootstrap/js/bootstrap.js"></script>
+        <script src="vendors/jquery/jquery.js"></script>
 
         <script>
             $(document).ready(function() {
