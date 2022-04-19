5b6978e06f33885063dba84e70a57c661f3b7790
RichardGcia <43912731+RichardGcia@users.noreply.github.com>
2022-04-19 17:13:11 -0500

    SUBIRLO DE NUEVO PORQUE MARIO LO BORRO TODO XDXD

--- index.php
+++ index.php
@@ -9,8 +9,47 @@
 
         <link rel="stylesheet" href="vendors/bootstrap/css/bootstrap.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
+        <style>
+            .form-input{
+                border-radius: 0;
+                border: none;
+                border-bottom: 2px solid #d9d9d9;
+            }
+
+            .form-input:hover{
+                border-color: #6f6f6f;
+            }
+
+            .form-input:focus, .form-input:active, .form-input:focus-visible{
+                box-shadow: none;
+                border-color: #9b9b9b;
+            }
+
+            .form-button{
+                border-radius: 0;
+                background: #204bce;
+                color: #fff;
+                font-size: 17px;
+                padding: 6px 6px;
+            }
+
+            .form-button:hover{
+                background: #5979f9;
+                color: #fff;
+                box-shadow: 0px 2px 5px #5979f9;
+            }
+
+            .form-button:focus, .form-button:active .form-button:focus-visible{
+                box-shadow: none;
+            }
+        </style>
     </head>
     <body>
+        <header class="header-principal">
+            <div class="container">
+
+            </div>
+        </header>
         <div class="container">
             <div class="d-flex justify-content-center m-5">
                 <div class="col-md-6">
@@ -19,17 +58,17 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="mb-3">
-                                <input id="uno" type="text" class="form-control" placeholder="introduce un numero" required>
+                                <input id="uno" type="text" class="form-control form-input" placeholder="introduce un numero" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="mb-3">
-                                <input id="dos" type="text" class="form-control" placeholder="introduce un numero" required>
+                                <input id="dos" type="text" class="form-control form-input" placeholder="introduce un numero" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="mb-3">
-                                <select id="operacion" name="operacion" class="form-select" required>
+                                <select id="operacion" name="operacion" class="form-select form-input" required>
                                     <option value="">Selecciona la operacion a realizar</option>
                                     <option value="suma">Sumar</option>
                                     <option value="resta">Restar</option>
@@ -40,12 +79,12 @@
                         </div>
                         <div class="col-md-6">
                             <div class="mb-3">
-                                <input id="result" type="text" class="form-control" readonly>
+                                <input id="result" type="text" class="form-control form-input" readonly>
                             </div>
                         </div>
                         <div class="col-md-12">
                             <div class="mb-3">
-                                <button id="btn" class="btn btn-success w-100">Ver resultado</button>
+                                <button id="btn" class="btn w-100 form-button">Ver resultado</button>
                             </div>
                         </div>
                     </div>
